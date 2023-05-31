fcd0d8956ba597611042c6419b0ac57e92abf1c8
Maksim Remin <reminmax@gmail.com>
2023-05-31 21:45:26 +0300

    RootNavigationGraph added, unused code deleted.

--- app/src/main/java/com/reminmax/mapkitapp/ui/MainActivity.kt
+++ app/src/main/java/com/reminmax/mapkitapp/ui/MainActivity.kt
@@ -3,13 +3,8 @@ package com.reminmax.mapkitapp.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.reminmax.mapkitapp.ui.navigation.RootNavigationGraph
 import com.reminmax.mapkitapp.ui.theme.MapKitAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,30 +12,10 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MapKitAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                RootNavigationGraph(
+                    navController = rememberNavController(),
+                )
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MapKitAppTheme {
-        Greeting("Android")
     }
 }
